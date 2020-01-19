bd14356d22214bd3cae36bfc4b1344ddfbc38ae1
talisonf <talison7737@gmail.com>
2020-01-19 17:42:06 -0300

    componentizando e finalizando estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
+import "./global.css"
+import "./App.css"
+import "./Sidebar.css"
+import "./Main.css"
+import { DevItem }from './component/DevItem';
+import { DevForm } from './component/DevForm';
+
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function getDevs(){
+      setDevs((await api.get("/devs")).data)
+    }
+    getDevs();
+
+  }, [])  
+
+  async function handleAddDev(data) {
+    
+    const response = await api.post("/devs", data)
+
+    setDevs([...devs, response.data]);
+  }
   return (
-    <h1>Hello World!</h1>
-    );
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit= {handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map( dev => (
+            <DevItem key={dev.id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+
+  );
 }
 
 export default App;
